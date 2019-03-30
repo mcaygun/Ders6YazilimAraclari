6fdbde9792f0727ccf009d720141bda2ef9f42b4
bprlab <bprlab@bprlab-iMac-97.local>
2019-03-30 14:01:04 +0300

    spring boot

---
+++ src/test/java/tr/edu/medipol/yazarac/ders6springboot/Ders6springbootApplicationTests.java
@@ -0,0 +1,16 @@
+package tr.edu.medipol.yazarac.ders6springboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Ders6springbootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
